e9eccbb0432879c49fe7208c5a2073acec9d3a4c
shukumar-542 <shukumar542@gmail.com>
2025-07-06 14:25:49 +0600

    Implement subscription plan container

--- app/Subscriptions.jsx
+++ app/Subscriptions.jsx
@@ -1,13 +1,66 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View } from "react-native";
 
 const Subscriptions = () => {
+  const plans = [
+    {
+      name: "Free Trail",
+      limit: "7",
+      price: "free",
+    },
+    {
+      name: "Monthly",
+      limit: "1",
+      price: "14.99",
+    },
+    {
+      name: "Yearly",
+      limit: "12",
+      price: "19.99",
+    },
+  ];
   return (
-    <View>
-      <Text>Subscriptions</Text>
+    <View style={styles.container}>
+      <View style={styles.planContainer}>
+        {plans?.map((plan) => {
+          return (
+            <View key={plan?.name} style={styles.planCard}>
+              <Text>{plan?.name}</Text>
+              <Text>{plan?.price}</Text>
+            </View>
+          );
+        })}
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Subscriptions
+export default Subscriptions;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    borderRadius: 10,
+  },
+  planContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+    gap: 16,
+  },
+  planCard: {
+    backgroundColor: "white",
+    width: "30%",
+    width: "100%",
+    borderRadius: 12,
+    textAlign: "center",
+    alignItems: "center",
+    padding: 24,
+    shadowRadius: 4,
+    elevation: 1,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.1,
+  },
+});
