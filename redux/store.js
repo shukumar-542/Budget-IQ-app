2cfd4ca3ed339fa8d6dd9bc67c428ffddb7db524
shukumar-542 <shukumar542@gmail.com>
2025-08-18 13:07:45 +0600

    Implement redux setup for the data fetching

---
+++ redux/store.js
@@ -0,0 +1,11 @@
+// redux/store.js
+import { configureStore } from '@reduxjs/toolkit';
+import { api } from './services/api';
+
+export const store = configureStore({
+  reducer: {
+    [api.reducerPath]: api.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware),
+});
